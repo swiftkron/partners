de705e3b966322d8f1bd9677b826ef0ce803f2f6
swiftkron <jeremy.kron@swiftpage.com>
2016-04-04 17:28:25 -0600

    activating utilty nav

---
+++ Partners/Scripts/utilitynav.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    // Common vars 
+    var width = $(window).width();
+    // Custom Functions
+    $(document).click(function () {
+        $('.uDropdown').hide();
+    });
+    $('.navLink').click(function (event) {
+        event.stopPropagation();
+        location.href = "http://www.act.com";
+    });
+    $('#au-nav .navLink').click(function (event) {
+        event.stopPropagation();
+        location.href = "/au";
+    });
+    $('#uk-nav .navLink').click(function (event) {
+        event.stopPropagation();
+        location.href = "/en-gb";
+    });
+    $('.uIntl').click(function (event) {
+        event.stopPropagation();
+        $('.lang').slideToggle(400);
+    });
+    $('.navToggle').click(function (event) {
+        event.stopPropagation();
+        $('nav').slideToggle(400);
+        $('.uAlt').hide();
+    });
+    $('.uToggle').click(function (event) {
+        event.stopPropagation();
+        $('.uAlt').slideToggle(400);
+    });
+});
